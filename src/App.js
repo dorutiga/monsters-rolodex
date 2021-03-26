eb1fb8c15784e6747a877bd84a76972ddb296542
DORU? IUGA <iuga.dorut165271@student.ubbcluj.ro>
2021-03-26 16:21:40 +0200

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import {Component} from 'react'
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component'
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      monsters :[],
+      cautare: ''
+    }
+  }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(raspuns => raspuns.json())
+   .then(users => this.setState({monsters: users}))
+    
+    
+  }
+  handleChange =(e) => {
+    this.setState({cautare: e.target.value})
+  }
+  render()  {
+    const { monsters, cautare} =this.state; //noile constante sunt copi ale state ului initial
+    const monstriiFiltrati = monsters.filter(monster=> 
+      monster.name.toLowerCase().includes(cautare.toLowerCase())
+      );
+      //filter are 2 argumente, primul contine informatiile, al doilea seteaza o regula, rezultat true or false
+      //functia includes() ->se verifica daca numele monstrului se gaseste in variabila searchfield
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+        placeholder = 'Search monster'
+        handleChange ={this.handleChange}
+        />
+        <CardList monsters = {monstriiFiltrati} > </CardList>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
